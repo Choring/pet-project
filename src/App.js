3168c1a2c91c365092d990d76a14117e3bf76430
Choring <skyhonor201@gmail.com>
2024-09-13 14:42:41 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-import './App.css';
-import { Routes, Route } from "react-router-dom";
-import { MainPage } from "./pages/main/MainPage";
-import { ListPage } from "./pages/List/ListPage";
-=======
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import NotFound from './components/NotFound';
->>>>>>> eunseon
-
+import { ListPage } from "./pages/List/ListPage";
 function App() {
   return (
     <Routes>
-<<<<<<< HEAD
-      <Route path="/" element={<MainPage />}></Route>
-      <Route path="/List" element={<ListPage />}></Route>
-=======
       <Route path='/' element={<Home />} />
+      <Route path='/list' element={<ListPage />} />
       <Route path='/*' element={<NotFound />} />
->>>>>>> eunseon
     </Routes>
   );
 }
