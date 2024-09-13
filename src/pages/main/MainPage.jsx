94b43787ea951eaefb93bdd3e51e10010f42fa8f
sangwon11 <dbstkd0408@gmail.com>
2024-09-13 19:15:01 +0900

    메인페이지ui

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const MainPage = () => {
+
+  const slides = [
+    {
+      url: 'https://health.chosun.com/site/data/img_dir/2022/05/24/2022052401445_0.jpg'
+    },
+    {
+      url: 'https://imgnn.seoul.co.kr/img/upload/2016/08/08/SSI_20160808134848_V.jpg'
+    },
+    {
+      url: 'https://img.seoul.co.kr/img/upload/2020/03/02/SSI_20200302104923_O2.jpg'
+    },
+    {
+      url: 'https://health.chosun.com/site/data/img_dir/2022/02/24/2022022401244_0.jpg'
+    },
+    {
+      url: 'https://blogs.cdc.gov/wp-content/uploads/sites/6/2018/05/HEADER-PHOTO_iStock-871996576.jpg'
+    },
+  ];
+
+  const Section = ({ title }) => (
+    <div>
+      <header className="text-2xl mt-5">{title}</header>
+      <div className="w-full flex flex-row">
+        {Array(7).fill(0).map((_, index) => (
+          <div key={index} className="w-44 h-56 mr-5 border-solid border divide-y">
+            <div className='h-4/6 w-full'></div>
+            <div>
+              <div className='text-lg'>시설명</div>
+              <div className='text-base'>주소</div>
+              <div className='text-base'>운영시간</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
-    <div>MainPage</div>
+    <div className='flex flex-col'>
+      <div className='mt-28 bg-orange w-full h-96 flex flex-col items-center justify-center'>
+        <div className="h-screen w-2/5 flex flex-col items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: `url(${slides[0].url})` }}>
+        </div>
+        <button className=' group-hover:block absolute top-[40%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 text-white cursor-pointer'>
+          &#10094;
+        </button>
+        <button className='absolute top-[40%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 text-white cursor-pointer'>
+          &#10095;
+        </button>
+      </div>
+
+      <div className=' flex flex-col items-center'>
+        <Section title="식당" />
+        <Section title="카페" />
+        <Section title="미술관" />
+        <Section title="펜션" />
+      </div>
+    </div>
   )
 }
