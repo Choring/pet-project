dbfb3795d2e0cfc2a15fd68e9b60aca8d491a46b
Choring <skyhonor201@gmail.com>
2024-09-14 16:04:47 +0900

    위도, 경도, 이름 수정

--- src/pages/List/ListPage.jsx
+++ src/pages/List/ListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ListPage.style.css';
 import { usePetFriendlyFacility } from '../../hooks/usePetFriendlyFacility';
 import { KakaoMap } from './component/KakaoMap';
@@ -9,20 +9,20 @@ import NotFound from "../../components/NotFound";
 import LoadingSpinner from "../../components/LoadingSpinner";
 
 export const ListPage = () => {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [query, setQuery] = useSearchParams();
   const category = query.get("category");
   const location = useLocation();
   const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
-  const [latitude, setLatitude] = useState(queryParams.get('lat'));
-  const [longitude, setLongitude] = useState(queryParams.get('lng'));
-  const [itemName, setItemName] = useState('');
   const {data, isLoading, isError, error} = useArtMuseum({page});
   const itemsPerPage = 10;
   const filteredData = data?.filter(
       (item) => (category === "문화시설") ? item.카테고리3 === "미술관" || item.카테고리3 === "박물관" : item.카테고리3 === category
     ) || [];
+  const [latitude, setLatitude] = useState(filteredData[0]?.위도);
+  const [longitude, setLongitude] = useState(filteredData[0]?.경도);
+  const [itemName, setItemName] = useState(filteredData[0]?.시설명);
 
   // 현재 페이지에 해당하는 데이터만 가져오기
   const offset = page * itemsPerPage;
@@ -30,7 +30,6 @@ export const ListPage = () => {
 
 
   const handleMap = (위도,경도,시설명) => {  
-    console.log(시설명);
     setLatitude(위도);
     setLongitude(경도);
     setItemName(시설명);
@@ -39,6 +38,13 @@ export const ListPage = () => {
   const handlePageClick = ({ selected }) => {
     setPage(selected + 1);
   };
+
+  useEffect(() => {
+    setLatitude(filteredData[0]?.위도);
+    setLongitude(filteredData[0]?.경도);
+    setItemName(filteredData[0]?.시설명);
+
+  },[filteredData])
 
   if (isError) return <NotFound />;
   if (isLoading) return <LoadingSpinner />;
