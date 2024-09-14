137e39c264ba3f354d582df74742a986e6a94714
Choring <skyhonor201@gmail.com>
2024-09-14 13:50:48 +0900

    네비,푸터 연결

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import Home from './pages/Home';
 import NotFound from './components/NotFound';
 import { ListPage } from "./pages/List/ListPage";
+import { MainPage } from "./pages/main/MainPage";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/list' element={<ListPage />} />
-      <Route path='/*' element={<NotFound />} />
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<MainPage />} />
+        <Route path='/list' element={<ListPage />} />
+        <Route path='/*' element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
